028c2cac69e8cd836b12875a4d181dd891bb2e9d
Luismichu <36201185+luismichu@users.noreply.github.com>
2020-04-20 00:34:51 +0200

    Create prov_comp.sql

---
+++ prov_comp.sql
@@ -0,0 +1,8 @@
+create table PROV_COMP(
+	CIF_PROVEEDOR varchar(50) primary key,
+    RAZ_PROVEEDOR varchar(100),
+    REG_NOTARIAL int,
+    SEG_RESPONSABILIDAD varchar(50),
+    SEG_IMPORTE float,
+    FEC_HOMOLOGACION date
+);
